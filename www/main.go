38ba0b990656ae859610d6805285b5af40a99543
Oleg Kozyrev <okozyrev@ozon.ru>
2021-09-15 19:07:45 +0300

    add struct user

--- www/main.go
+++ www/main.go
@@ -5,8 +5,20 @@ import (
 	"net/http"
 )
 
+type user struct {
+	name string
+	age uint16
+	balance int64
+}
+
+func (u *user) getUserInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has %d$.", u.name, u.age, u.balance)
+}
+
 func mainPage(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Main page")
+	testUser := user{name: "Oleg", age: 26, balance: 10000}
+
+	fmt.Fprintf(w, testUser.getUserInfo())
 }
 
 func aboutPage(w http.ResponseWriter, req *http.Request) {
